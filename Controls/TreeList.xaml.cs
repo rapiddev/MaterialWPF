b7cf1d4bada92a91217a456b52833ed1fa08d16f
Pomianowski <kontakt@rapiddev.pl>
2021-03-09 13:55:46 +0100

    Add license

--- Controls/TreeList.xaml.cs
+++ Controls/TreeList.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) David Anson
+// Copyright (C) Leszek Pomianowski and MaterialWPF Contributors.
+// All Rights Reserved.
 
+using System.Windows.Controls;
 namespace MaterialWPF.Controls
 {
     public class ScheduleItem
